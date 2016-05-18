0ad45fd647c910383a7a599afd8b701603174c7d
Dave Liddament <dave@lampbristol.com>
2016-05-18 13:41:38 +0100

    Added code to make 4 work
    Summary:
    --------
    
    Again we've taken the approach of the simplest possible way of getting the tests working.
    this was done by treating 4 as an exception case.
    
    Next step:
    ----------
    
    Create a failing test for input of 5, expected value 'V'.

--- src/RomanNumeralsConverter.php
+++ src/RomanNumeralsConverter.php
@@ -17,6 +17,9 @@ class RomanNumeralsConverter
      */
     public function convert($number)
     {
+        if ($number == 4) {
+            return 'IV';
+        }
         return str_repeat('I', $number);
     }
 
